aef54339263e39d082ce6d1fc5d0de2223460dc2
sellectuwa <szymonpolny@outlook.com>
2021-09-05 01:13:15 +0200

    Auth in settings view

--- pages/dashboard/settings.js
+++ pages/dashboard/settings.js
@@ -1,7 +1,29 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect, useContext } from 'react';
+import { Center, Spinner } from '@chakra-ui/react';
+
+import AuthContext from '../../context/authContext';
 import DashboardLayout from '../../components/Dashboard/DashboardLayout';
 
 const Settings = () => {
+  const router = useRouter();
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (!user.isLoggedIn) {
+      router.push('/');
+    }
+  }, [user]);
+
+  if (!user.isLoggedIn) {
+    return (
+      <Center h="100vh">
+        <Spinner size="xl" />
+      </Center>
+    );
+  }
+
   return (
     <DashboardLayout>
       <Head>
